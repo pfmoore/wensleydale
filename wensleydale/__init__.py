39631a0626396a6181f6c1494d7f35628012d7c2
Paul Moore <p.f.moore@gmail.com>
2013-12-20 14:05:43 +0000

    Get functional tests working (commit/rollback)

--- wensleydale/__init__.py
+++ wensleydale/__init__.py
@@ -1,7 +1,17 @@
+class DatabaseError(Exception): pass
+
 class DB:
     def __init__(self, engine):
         self.engine = engine
+        self.packages = set()
+        self.old_packages = set()
     def init(self):
         pass
+    def commit(self):
+        self.old_packages = set(self.packages)
+    def rollback(self):
+        self.packages = set(self.old_packages)
     def load(self, package, version=None):
-        pass
+        if (package, version) in self.packages:
+            raise DatabaseError("Package {}/{} already present".format(package, version))
+        self.packages.add((package, version))
